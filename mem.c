f391903f7b847755aaca846652383fbed1505147
yel <yel@localhost.localdomain>
2025-06-09 10:34:50 +0800

    pointer to header metadata

--- mem.c
+++ mem.c
@@ -1,15 +1,22 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#include "mem.h"
 
 void *malloc(size_t size) 
 {
+	/*
+	 * Allocating Memory without traversal
+	 */
 	void *block;
+	header_t *header; 
 
 	/* Moves the heap by the number of size*/
 	block = sbrk(size);
 
 	/* On failure, sbrk() returns (void*) -1 */ 
-	if (block == (void*) -1)
-		return NULL; 
-	return block;
+	if (block == (void*) -1) {
+		return NULL;
+	}
+	header->s.size = size;
+	header->s.is_free = 0; /* 0 as the memory was used, 1 as free */
 }
