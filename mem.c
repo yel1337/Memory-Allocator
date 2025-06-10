da17300442b07dd969852ba54592298b19235257
yel <yel@localhost.localdomain>
2025-06-10 10:57:51 +0800

    Added pointer to point to usable memory

--- mem.c
+++ mem.c
@@ -2,15 +2,17 @@
 #include <stdlib.h> 
 #include "mem.h"
 
+header_t *head, *tail; 
 void *malloc(size_t size) 
-{
-	/*
-	 * Allocating Memory without traversal
-	 */
-	void *block;
+{ 
+	void *block
 	header_t *header; 
-
-	/* Moves the heap by the number of size*/
+	
+	/*
+	 * Take a chunk of memory to heap with the given size
+	 * 
+	 * sbrk() allocates in the heap
+	 */
 	block = sbrk(size);
 
 	/* On failure, sbrk() returns (void*) -1 */ 
@@ -18,5 +20,22 @@ void *malloc(size_t size)
 		return NULL;
 	}
 	header->s.size = size;
-	header->s.is_free = 0; /* 0 as the memory was used, 1 as free */
+	header->s.is_free = 0; /* 0 as the memory is now in use, 1 as free */	
+	header->s.next = NULL;
+
+	/* If there's no head in linked list then point it to the current header*/
+	if (!head) {
+		head = header; 
+	}
+
+	/* If there is an existing tail then point tail to the next header */
+	if (tail) {
+		tail->s.next = header; 
+	}
+
+	/* Point tail to the current header */
+	tail = header; 
+	
+	/* Return a pointer to header and append by 1 to return usable memory*/ 
+	return (void*)(header + 1);
 }
